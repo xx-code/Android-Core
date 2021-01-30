5200df6069171b4e8e2acce4449f03251c6e77f1
Auguste Junior <akaaugustejunior@gmail.com>
2021-01-30 13:05:10 +0000

    hult

--- app/src/main/java/com/learning/androidcore/home.kt
+++ app/src/main/java/com/learning/androidcore/home.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -14,6 +16,8 @@ import androidx.navigation.fragment.findNavController
 class home : Fragment() {
 
     lateinit var goButton: Button
+    lateinit var nameInput: EditText
+    lateinit var emailInput: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +29,14 @@ class home : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        nameInput = view.findViewById(R.id.nameEdite)
+        emailInput = view.findViewById(R.id.emailEdit)
+
         goButton = view.findViewById(R.id.gogButton)
 
         goButton.setOnClickListener {
-            view.findNavController().navigate(R.id.action_home2_to_second)
+            val bundle= bundleOf("email" to emailInput.text.toString(), "name" to nameInput.text.toString())
+            view.findNavController().navigate(R.id.action_home2_to_second, bundle)
         }
 
         return view
